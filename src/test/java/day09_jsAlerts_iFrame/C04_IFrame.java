524f090a796946d52b1c0e1107c220e39a43b7de
bulutluoz <abbulutluoz@gmail.com>
2024-05-21 19:01:17 +0200

    day09 sonu

---
+++ src/test/java/day09_jsAlerts_iFrame/C04_IFrame.java
@@ -0,0 +1,51 @@
+package day09_jsAlerts_iFrame;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utilities.ReusableMethods;
+import utilities.TestBase;
+
+public class C04_IFrame extends TestBase {
+
+    @Test
+    public void test01(){
+        // 1 ) https://the-internet.herokuapp.com/iframe adresine gidin.
+        driver.get("https://the-internet.herokuapp.com/iframe");
+        // 2 ) Bir metod olusturun: iframeTest
+        //	- “An IFrame containing….” textinin erisilebilir oldugunu test edin ve  konsolda yazdirin.
+
+        WebElement anIframeYaziElementi = driver.findElement(By.tagName("h3"));
+
+        Assertions.assertTrue(anIframeYaziElementi.isEnabled());
+
+        System.out.println(anIframeYaziElementi.getText());
+
+        ReusableMethods.bekle(1);
+        driver.findElement(By.xpath("//div[@aria-label='Close']"))
+                .click();
+        ReusableMethods.bekle(2);
+
+        //	- Text Box’a “Merhaba Dunya!” yazin.
+            // Textbox'in iframe icinde oldugunu gorduk
+            // oncelikle o iframe'e gecis yapalim
+
+
+        // *** web sayfasi textbox'a yazmaya izin vermediginden IPTAL
+
+
+        WebElement iFrameElementi = driver.findElement(By.xpath("//iframe[@title='Rich Text Area']"));
+        driver.switchTo().frame(iFrameElementi);
+        ReusableMethods.bekle(2);
+        WebElement textBox = driver.findElement(By.tagName("p"));
+        textBox.clear();
+        textBox.sendKeys("Merhaba Dunya!");
+
+        //	- TextBox’in altinda bulunan “Elemental Selenium” linkinin gorunur oldugunu dogrulayin
+        //    ve  yazisini konsolda yazdirin.
+
+
+        ReusableMethods.bekle(3);
+    }
+}
