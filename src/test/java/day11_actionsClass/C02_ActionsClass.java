54bf093963b5e2c3a27571cf4f18680e7188ff38
bulutluoz <abbulutluoz@gmail.com>
2024-05-22 19:41:56 +0200

    day11 sonu

---
+++ src/test/java/day11_actionsClass/C02_ActionsClass.java
@@ -0,0 +1,16 @@
+package day11_actionsClass;
+
+import org.junit.jupiter.api.Test;
+import utilities.TestBase;
+
+public class C02_ActionsClass extends TestBase {
+
+    @Test
+    public void test01(){
+
+        //1- https://testotomasyonu.com/click sitesine gidin
+        //2- “DGI Drones” uzerinde sag click yapin
+        //3- Alert’te cikan yazinin “Tebrikler!... Sağ click yaptınız.” oldugunu test edin.
+        //4- Tamam diyerek alert’i kapatalim
+    }
+}
